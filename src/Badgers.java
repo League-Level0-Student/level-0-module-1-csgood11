622b38cdae6b2cb0f4cc294c2b7ebdc7a0562643
plooba <league@WTS13.attlocal.net>
2017-10-24 17:00:32 -0700

    My Ages, Badges (ip)

---
+++ src/Badgers.java
@@ -0,0 +1,8 @@
+
+public class Badgers {
+	public static void main(String[] args) {
+		for (int count = 0; count < 11; count++) {
+			System.out.println("Badger");
+		}
+	}
+}
---
+++ src/MyAges.java
@@ -0,0 +1,8 @@
+
+public class MyAges {
+	public static void main(String[] args) {
+		for (int count = 0; count < 16; count++) {
+			System.out.println(count);
+		}
+	}
+}
